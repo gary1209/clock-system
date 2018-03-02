22760a724e7788554543a6cca7e489905645c2d6
gary1209 <b01608011@ntu.edu.tw>
2018-03-02 15:46:13 +0800

    upload test 1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
 <html>
     <head><meta charset="UTF-8"></head>
     <body>
-        <h1>S3 upload example</h1>
+        
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
@@ -21,9 +21,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
 <?php } catch(Exception $e) { ?>
         <p>Upload error :(</p>
 <?php } } ?>
-        <h2>Upload a file</h2>
+        
         <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
         </form>
     </body>
 </html>
+
