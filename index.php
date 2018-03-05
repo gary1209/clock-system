e6246ab4082d3ec0c386dcf36408ce4123671539
gary1209 <b01608011@ntu.edu.tw>
2018-03-05 17:43:56 +0800

    upload ajax  4'

--- index.php
+++ index.php
@@ -67,3 +67,5 @@ imgFile.onchange = function() {
 	});
 }
 </script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
