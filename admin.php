a7646985a7bb6f899f95272d3e5254bec1a750e3
gary1209 <b01608011@ntu.edu.tw>
2018-03-12 17:16:49 +0800

    admin page fixed

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
   <link rel="stylesheet" href="css/bootstrap.css" />
   <!-- <link rel="stylesheet" href="assets/css/main.css"/> -->
-  <link rel="stylesheet" type="text/css" href="datatables/DataTables-1.10.16/css/jquery.dataTables.css"/>
+  <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css"/>
  
   <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.0.0/jq-3.2.1/dt-1.10.16/datatables.min.js"></script>
 
